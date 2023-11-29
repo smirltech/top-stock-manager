4c21c1add280842e1ff3faffa743c7cc2ebefd9c
Francis Nduba Numbi <francisnnumbi@yandex.com>
2023-11-29 07:39:17 +0200

    fix client bottomsheet

--- lib/application/ui/clients/clients_screen.dart
+++ lib/application/ui/clients/clients_screen.dart
@@ -33,6 +33,7 @@ class _ClientsScreenState extends State<ClientsScreen> {
                   ),
                   OutlinedButton.icon(
                     onPressed: () {
+                      ClientsController.to.client.value = null;
                       clientAdd();
                     },
                     style: OutlinedButton.styleFrom(
@@ -151,116 +152,153 @@ class _ClientsScreenState extends State<ClientsScreen> {
     Get.bottomSheet(
       Container(
         margin: const EdgeInsets.symmetric(horizontal: 5.0),
-        child: Form(
-            key: clientAddFormKey,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Center(
-                    child: Text(
-                      title.tr,
-                      style: const TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    initialValue: client['name'],
-                    validator: (va) {
-                      if (va!.isEmpty) {
-                        return "Name must not be empty".tr;
-                      } else if (va.length < 3) {
-                        return "Length of name must be 3 characters and above";
-                      }
-                      return null;
-                    },
-                    onSaved: (String? value) {
-                      client['name'] = value.toString();
-                    },
-                    decoration: InputDecoration(
-                      hintText: "Client Name".tr,
-                      labelText: "Name".tr,
-                      border: const OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    initialValue: client['description'],
-                    onSaved: (String? value) {
-                      client['description'] = value.toString();
-                    },
-                    decoration: InputDecoration(
-                      hintText: "Client Description".tr,
-                      labelText: "Description".tr,
-                      border: const OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Wrap(
+          children: [
+            Form(
+                key: clientAddFormKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
                     children: [
-                      Flexible(
-                        child: TextFormField(
-                          initialValue: client['sex'].toString(),
-                          onSaved: (String? value) {
-                            client['sex'] = value.toString();
-                          },
-                          decoration: InputDecoration(
-                            hintText: "Sex".tr,
-                            labelText: "Sex".tr,
-                            border: const OutlineInputBorder(),
-                          ),
+                      Center(
+                        child: Text(
+                          title.tr,
+                          style: const TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
                         ),
                       ),
                       const SizedBox(
-                        width: 10.0,
+                        height: 20,
                       ),
-                      Flexible(
-                        child: TextFormField(
-                          initialValue: client['contact'],
-                          onSaved: (String? value) {
-                            client['contact'] = value.toString();
-                          },
-                          decoration: InputDecoration(
-                            hintText: "Contact".tr,
-                            labelText: "Contact".tr,
-                            border: const OutlineInputBorder(),
+                      TextFormField(
+                        initialValue: client['name'],
+                        validator: (va) {
+                          if (va!.isEmpty) {
+                            return "Name must not be empty".tr;
+                          } else if (va.length < 3) {
+                            return "Length of name must be 3 characters and above";
+                          }
+                          return null;
+                        },
+                        onSaved: (String? value) {
+                          client['name'] = value.toString();
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Client Name".tr,
+                          labelText: "Name".tr,
+                          border: const OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        initialValue: client['description'],
+                        onSaved: (String? value) {
+                          client['description'] = value.toString();
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Client Description".tr,
+                          labelText: "Description".tr,
+                          border: const OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Flexible(
+                            child: DropdownButtonFormField(
+                              value: client['sex'].toString(),
+                              decoration: InputDecoration(
+                                hintText: "Sex".tr,
+                                labelText: "Sex".tr,
+                                border: const OutlineInputBorder(),
+                              ),
+                              items: [
+                                DropdownMenuItem(
+                                  value: "",
+                                  child: Text(
+                                    "UNDEFINED".tr,
+                                  ),
+                                ),
+                                DropdownMenuItem(
+                                  value: "M",
+                                  child: Text(
+                                    "Male".tr,
+                                  ),
+                                ),
+                                DropdownMenuItem(
+                                  value: "F",
+                                  child: Text(
+                                    "Female".tr,
+                                  ),
+                                ),
+                              ],
+                              onChanged: (value) {
+                                client['sex'] = value.toString();
+                              },
+                            ),
                           ),
+                          /*  Flexible(
+                            child: TextFormField(
+                              initialValue: client['sex'].toString(),
+                              onSaved: (String? value) {
+                                client['sex'] = value.toString();
+                              },
+                              decoration: InputDecoration(
+                                hintText: "Sex".tr,
+                                labelText: "Sex".tr,
+                                border: const OutlineInputBorder(),
+                              ),
+                            ),
+                          ),*/
+                          const SizedBox(
+                            width: 10.0,
+                          ),
+                          Flexible(
+                            child: TextFormField(
+                              initialValue: client['contact'],
+                              onSaved: (String? value) {
+                                client['contact'] = value.toString();
+                              },
+                              decoration: InputDecoration(
+                                hintText: "Contact".tr,
+                                labelText: "Contact".tr,
+                                border: const OutlineInputBorder(),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Center(
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            if (!clientAddFormKey.currentState!.validate()) {
+                              return;
+                            }
+                            clientAddFormKey.currentState!.save();
+                            ClientsController.to.saveClient(client);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: kWhite,
+                            backgroundColor: kWarning,
+                          ),
+                          label: Text('Save'.tr),
+                          icon: const Icon(Icons.save),
                         ),
                       ),
                     ],
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Center(
-                    child: ElevatedButton.icon(
-                      onPressed: () {
-                        if (!clientAddFormKey.currentState!.validate()) {
-                          return;
-                        }
-                        clientAddFormKey.currentState!.save();
-                        ClientsController.to.saveClient(client);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: kWhite,
-                        backgroundColor: kWarning,
-                      ),
-                      label: Text('Save'.tr),
-                      icon: const Icon(Icons.save),
-                    ),
-                  ),
-                ],
-              ),
-            )),
+                )),
+          ],
+        ),
       ),
       backgroundColor: kWhite,
       ignoreSafeArea: true,
